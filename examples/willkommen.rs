3587764a9cf3012461b597d1b8863c2f962d1a26
felix <felixwochnick@gmail.com>
2021-04-05 16:21:30 +0200

    create examples folder

---
+++ examples/willkommen.rs
@@ -0,0 +1,12 @@
+/*
+    Beispielprogramm:
+    Robot läuft 1 Runde
+*/
+
+var zähler = 4
+wiederhole (zähler):
+    wiederhole_solange (nichtIstWand):
+        schritt
+    *wiederhole
+    linksdrehen
+*wiederhole
