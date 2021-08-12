df15c3f6996a810e48bce02ec3bfb50c3137b2d5
felix-07-11 <felixwochnick@gmail.de>
2021-08-12 20:03:23 +0200

    select Changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
     root: true,
     env: {
         node: true,
@@ -25,34 +24,6 @@ module.exports = {
                 endOfLine: 'auto',
             },
         ],
-=======
-
-	root: true,
-	env: {
-		node: true,
-	},
-	extends: [
-		'plugin:vue/essential',
-		'eslint:recommended',
-		'@vue/typescript/recommended',
-		'prettier',
-		// '@vue/prettier',
-		// '@vue/prettier/@typescript-eslint',
-	],
-	plugins: ['prettier'],
-	parserOptions: {
-		ecmaVersion: 2020,
-	},
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'prettier/prettier': [
-			'error',
-			{
-				endOfLine: 'auto',
-			},
-		],
->>>>>>> 0369535d8e24f38cd4bfe72352f41f78f5568264
 
         allowIndentationTabs: 0,
     },
--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,4 @@ new Vue({
     store,
     vuetify,
     render: (h) => h(App),
-<<<<<<< HEAD
 }).$mount('#app');
-=======
-}).$mount('#app')
->>>>>>> 0369535d8e24f38cd4bfe72352f41f78f5568264
